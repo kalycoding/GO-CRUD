4d6da7da45cf40cc5806d5bb7aa8d110a578e781
kalycoding <muhdkhalifa1@yahoo.com>
2021-11-03 22:54:21 +0100

    Get blogpost by id

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 type BlogPost struct {
 	ID          string `json:"id"`
 	Title       string `json:"title"`
@@ -7,4 +11,27 @@ type BlogPost struct {
 	Description string `json:"description"`
 }
 
-var blog = []BlogPost{}
+var blog = []BlogPost{
+	{ID: "1", Title: "Learning Go", Author: "Kalycoding", Description: "My Path to Learning Go"},
+}
+
+func getBlogPosts(c *gin.Context) {
+	c.JSON(200, blog)
+}
+
+func getBlogPostById(c *gin.Context) {
+	for i := 0; i < len(blog); i++ {
+		if blog[i].ID == c.Param("id") {
+			c.JSON(200, blog[i])
+		}
+	}
+	c.JSON(404, gin.H{
+		"errorMessage": "Not found",
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/posts", getBlogPosts)
+	r.Run()
+}
